5878cb6fcf39bcab796eabbdf8de360714c22d6a
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-08-12 18:19:58 +0500

    added try catch while connecting with database

--- src/database_tasks.py
+++ src/database_tasks.py
@@ -1,9 +1,12 @@
+from contextlib import asynccontextmanager
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from contextlib import asynccontextmanager
+
 from src.config import DATABASE_URL
 
-task_engine = create_async_engine(DATABASE_URL, echo=True)
+task_engine = create_async_engine(DATABASE_URL, echo=True, pool_size=50,
+                                  max_overflow=20)
 TaskSessionLocal = sessionmaker(
     bind=task_engine,
     class_=AsyncSession,
@@ -12,7 +15,14 @@ TaskSessionLocal = sessionmaker(
 
 Base = declarative_base()
 
+
 @asynccontextmanager
 async def get_task_db():
     async with TaskSessionLocal() as session:
-        yield session
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
