e1b36a01a2f1bda78acf19aa8a8a6883b980563b
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-08-15 12:54:48 +0500

    increases pool size

--- src/database_tasks.py
+++ src/database_tasks.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from src.config import DATABASE_URL
 
-task_engine = create_async_engine(DATABASE_URL, echo=True, pool_size=50,
-                                  max_overflow=20)
+task_engine = create_async_engine(DATABASE_URL, echo=True, pool_size=100, max_overflow=200)
 TaskSessionLocal = sessionmaker(
     bind=task_engine,
     class_=AsyncSession,
