9d245ff21c1845d1da6577a585c09c3a85f5883a
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-08-19 12:01:37 +0500

    session for simple postgres database

--- src/database_tasks.py
+++ src/database_tasks.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -12,8 +13,23 @@ TaskSessionLocal = sessionmaker(
     expire_on_commit=False
 )
 
-Base = declarative_base()
+# mysql
+DATABASE_URL="mysql+mysqlconnector://root:password@localhost:3306/database_name"
+task_engine_2 = create_engine(DATABASE_URL, echo=True, pool_size=20, max_overflow=30, pool_timeout=30)
+TaskSessionLocal_2 = sessionmaker(
+    bind=task_engine_2,
+    expire_on_commit=False
+)
 
+# postgres
+DATABASE_URL = "postgresql+psycopg2://developer:DeltaCompute123@rococo-db-server-postgres-aurora.cluster-c3y444mm80qj.eu-west-1.rds.amazonaws.com/postgres"
+task_engine_3 = create_engine(DATABASE_URL, echo=True, pool_size=20, max_overflow=30, pool_timeout=30)
+TaskSessionLocal_3 = sessionmaker(
+    bind=task_engine_3,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
 
 @asynccontextmanager
 async def get_task_db():
