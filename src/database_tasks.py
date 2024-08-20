55f6e24fe20ce94455b134a86527c22690f540fa
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-08-20 15:01:59 +0500

    read database url from config file

--- src/database_tasks.py
+++ src/database_tasks.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from src.config import DATABASE_URL
+from src.config import DATABASE_URL, DATABASE_URL_SYNC
 
 task_engine = create_async_engine(DATABASE_URL, echo=True, pool_size=20, max_overflow=30, pool_timeout=30)
 TaskSessionLocal = sessionmaker(
@@ -13,19 +13,10 @@ TaskSessionLocal = sessionmaker(
     expire_on_commit=False
 )
 
-# mysql
-# DATABASE_URL="mysql+mysqlconnector://root:password@localhost:3306/database_name"
-# task_engine_2 = create_engine(DATABASE_URL, echo=True, pool_size=20, max_overflow=30, pool_timeout=30)
-# TaskSessionLocal_2 = sessionmaker(
-#     bind=task_engine_2,
-#     expire_on_commit=False
-# )
-
-# postgres
-DATABASE_URL = "postgresql+psycopg2://developer:DeltaCompute123@rococo-db-server-postgres-aurora.cluster-c3y444mm80qj.eu-west-1.rds.amazonaws.com/postgres"
-task_engine_3 = create_engine(DATABASE_URL, echo=True, pool_size=20, max_overflow=30, pool_timeout=30)
-TaskSessionLocal_3 = sessionmaker(
-    bind=task_engine_3,
+# postgres without asynchronous
+task_engine_ = create_engine(DATABASE_URL_SYNC, echo=True, pool_size=20, max_overflow=30, pool_timeout=30)
+TaskSessionLocal_ = sessionmaker(
+    bind=task_engine_,
     expire_on_commit=False
 )
 
