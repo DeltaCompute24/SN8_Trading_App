d54b6213bba08e284bc1d4dddafe7540b91b997b
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-08-21 17:13:15 +0500

    close transaction service method

--- src/services/trade_service.py
+++ src/services/trade_service.py
@@ -1,19 +1,24 @@
+from datetime import datetime
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.sql import and_, text
-from src.models.transaction import Transaction
-from src.schemas.transaction import TransactionCreate
-from src.schemas.monitored_position import MonitoredPositionCreate
-from datetime import datetime
 from sqlalchemy.sql import func
 
-async def create_transaction(db: AsyncSession, transaction_data: TransactionCreate, entry_price: float, operation_type: str,
+from src.models.transaction import Transaction
+from src.schemas.monitored_position import MonitoredPositionCreate
+from src.schemas.transaction import TransactionCreate
+
+
+async def create_transaction(db: AsyncSession, transaction_data: TransactionCreate, entry_price: float,
+                             operation_type: str,
                              position_id: int = None, trade_order: int = None, cumulative_leverage: float = None,
                              cumulative_stop_loss: float = None, cumulative_take_profit: float = None,
                              cumulative_order_type: str = None, status: str = "OPEN", close_time: datetime = None,
                              close_price: float = None, profit_loss: float = None):
     if operation_type == "initiate":
-        max_position_id = await db.scalar(select(func.max(Transaction.position_id)).filter(Transaction.trader_id == transaction_data.trader_id))
+        max_position_id = await db.scalar(
+            select(func.max(Transaction.position_id)).filter(Transaction.trader_id == transaction_data.trader_id))
         position_id = (max_position_id or 0) + 1
         trade_order = 1
         cumulative_leverage = transaction_data.leverage
@@ -21,7 +26,8 @@ async def create_transaction(db: AsyncSession, transaction_data: TransactionCrea
         cumulative_take_profit = transaction_data.take_profit
         cumulative_order_type = transaction_data.order_type
     else:
-        max_trade_order = await db.scalar(select(func.max(Transaction.trade_order)).filter(Transaction.position_id == position_id))
+        max_trade_order = await db.scalar(
+            select(func.max(Transaction.trade_order)).filter(Transaction.position_id == position_id))
         trade_order = (max_trade_order or 0) + 1
 
     new_transaction = Transaction(
@@ -50,6 +56,31 @@ async def create_transaction(db: AsyncSession, transaction_data: TransactionCrea
     await db.commit()
     await db.refresh(new_transaction)
     return new_transaction
+
+
+async def close_transaction(db: AsyncSession, order_id, close_price):
+    close_time = datetime.utcnow()
+    statement = text("""
+            UPDATE transactions
+            SET operation_type = :operation_type, 
+                status = :status, 
+                close_time = :close_time, 
+                close_price = :close_price
+            WHERE order_id = :order_id
+        """)
+
+    await db.execute(
+        statement,
+        {
+            "operation_type": "close",
+            "status": "CLOSED",
+            "close_time": close_time,
+            "close_price": close_price,
+            "order_id": order_id
+        }
+    )
+    await db.commit()
+
 
 async def update_monitored_positions(db: AsyncSession, position_data: MonitoredPositionCreate):
     await db.execute(
@@ -83,6 +114,7 @@ async def update_monitored_positions(db: AsyncSession, position_data: MonitoredP
     )
     await db.commit()
 
+
 async def get_open_position(db: AsyncSession, trader_id: int, trade_pair: str) -> Transaction:
     latest_position_id = await db.scalar(
         select(func.max(Transaction.position_id)).where(
@@ -111,6 +143,7 @@ async def get_open_position(db: AsyncSession, trader_id: int, trade_pair: str) -
 
     return None
 
+
 async def get_latest_position(db: AsyncSession, trader_id: int, trade_pair: str) -> Transaction:
     latest_position_id = await db.scalar(
         select(func.max(Transaction.position_id)).where(
@@ -133,7 +166,9 @@ async def get_latest_position(db: AsyncSession, trader_id: int, trade_pair: str)
     ).order_by(Transaction.trade_order.desc()))
     return result.scalars().first()
 
-def calculate_profit_loss(entry_price: float, current_price: float, leverage: float, order_type: str, asset_type: str) -> float:
+
+def calculate_profit_loss(entry_price: float, current_price: float, leverage: float, order_type: str,
+                          asset_type: str) -> float:
     fee = calculate_fee(leverage, asset_type)
     if order_type == "LONG":
         price_difference = (current_price - entry_price) * leverage
@@ -142,6 +177,7 @@ def calculate_profit_loss(entry_price: float, current_price: float, leverage: fl
     net_profit = price_difference - fee
     profit_loss_percent = (net_profit / (entry_price * leverage)) * 100
     return profit_loss_percent
+
 
 def calculate_fee(leverage: float, asset_type: str) -> float:
     return (0.00007 * leverage) if asset_type == 'forex' else (0.002 * leverage)
