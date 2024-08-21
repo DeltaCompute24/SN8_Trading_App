06d845f705e3b6ad30d29e50112209896411d25d
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-08-21 17:15:30 +0500

    resolve error in long and short order and call close transaction before creating new transaction

--- src/api/routes/adjust_position.py
+++ src/api/routes/adjust_position.py
@@ -1,22 +1,25 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql import text  # Add this import
+from sqlalchemy.sql import text
+
 from src.database import get_db
-from src.schemas.transaction import TransactionCreate, TradeResponse
 from src.schemas.monitored_position import MonitoredPositionCreate
-from src.services.trade_service import create_transaction, get_latest_position, update_monitored_positions
+from src.schemas.transaction import TransactionCreate
+from src.services.trade_service import create_transaction, get_open_position, update_monitored_positions, \
+    close_transaction
 from src.utils.logging import setup_logging
 from src.utils.websocket_manager import websocket_manager
 
 logger = setup_logging()
 router = APIRouter()
 
-@router.post("/adjust-position/", response_model=TradeResponse)
+
+@router.post("/adjust-position/", response_model=dict)
 async def adjust_position_endpoint(position_data: TransactionCreate, db: AsyncSession = Depends(get_db)):
     logger.info(f"Adjusting position for trader_id={position_data.trader_id} and trade_pair={position_data.trade_pair}")
 
     # Get the latest transaction record for the given trader and trade pair
-    latest_position = await get_latest_position(db, position_data.trader_id, position_data.trade_pair)
+    latest_position = await get_open_position(db, position_data.trader_id, position_data.trade_pair)
     if not latest_position:
         logger.error("No open position found for this trade pair and trader")
         raise HTTPException(status_code=404, detail="No open position found for this trade pair and trader")
@@ -26,7 +29,7 @@ async def adjust_position_endpoint(position_data: TransactionCreate, db: AsyncSe
 
         # Calculate new leverage based on the cumulative order type
         if (latest_position.cumulative_order_type == 'LONG' and position_data.order_type.upper() == 'LONG') or \
-           (latest_position.cumulative_order_type == 'SHORT' and position_data.order_type.upper() == 'SHORT'):
+                (latest_position.cumulative_order_type == 'SHORT' and position_data.order_type.upper() == 'SHORT'):
             new_leverage = latest_position.cumulative_leverage + position_data.leverage
         else:
             new_leverage = latest_position.cumulative_leverage - position_data.leverage
@@ -48,25 +51,37 @@ async def adjust_position_endpoint(position_data: TransactionCreate, db: AsyncSe
         cumulative_stop_loss = position_data.stop_loss
         cumulative_take_profit = position_data.take_profit
 
-        logger.info(f"Cumulative leverage: {cumulative_leverage}, Cumulative stop loss: {cumulative_stop_loss}, Cumulative take profit: {cumulative_take_profit}, Cumulative order type: {cumulative_order_type}")
+        logger.info(
+            f"Cumulative leverage: {cumulative_leverage}, Cumulative stop loss: {cumulative_stop_loss}, Cumulative take profit: {cumulative_take_profit}, Cumulative order type: {cumulative_order_type}")
 
         # Submit the adjustment signal
-        adjustment_submitted = await websocket_manager.submit_trade(position_data.trader_id, position_data.trade_pair, position_data.order_type, position_data.leverage)
+        adjustment_submitted = await websocket_manager.submit_trade(position_data.trader_id, position_data.trade_pair,
+                                                                    position_data.order_type, position_data.leverage)
         if not adjustment_submitted:
             logger.error("Failed to submit adjustment")
             raise HTTPException(status_code=500, detail="Failed to submit adjustment")
 
         logger.info("Adjustment submitted successfully")
 
+        # Connect and subscribe to the WebSocket
+        logger.info(f"Connecting to WebSocket for asset type {latest_position.asset_type}")
+        websocket = await websocket_manager.connect(latest_position.asset_type)
+        subscription_response = await websocket_manager.subscribe(latest_position.trade_pair)
+        logger.info(f"Subscription response: {subscription_response}")
+
+        # Close Old Transaction
+        close_price = await websocket_manager.listen_for_initial_price()
+        await close_transaction(db, latest_position.order_id, close_price)
+
         # Create a new transaction record with updated values
         new_transaction = await create_transaction(
-            db, position_data, entry_price=latest_position.entry_price, operation_type="adjust", 
+            db, position_data, entry_price=latest_position.entry_price, operation_type="adjust",
             position_id=latest_position.position_id,
             trade_order=latest_position.trade_order + 1,
             cumulative_leverage=cumulative_leverage,
             cumulative_stop_loss=cumulative_stop_loss,
             cumulative_take_profit=cumulative_take_profit,
-            cumulative_order_type=position_data.order_type.upper()
+            cumulative_order_type=cumulative_order_type
         )
 
         # Remove old monitored position
@@ -78,11 +93,11 @@ async def adjust_position_endpoint(position_data: TransactionCreate, db: AsyncSe
 
         # Update the monitored_positions table with the new transaction
         await update_monitored_positions(
-            db, 
+            db,
             MonitoredPositionCreate(
-                position_id=new_transaction.position_id, 
-                order_id=new_transaction.trade_order, 
-                trader_id=new_transaction.trader_id, 
+                position_id=new_transaction.position_id,
+                order_id=new_transaction.trade_order,
+                trader_id=new_transaction.trader_id,
                 trade_pair=new_transaction.trade_pair,
                 cumulative_leverage=new_transaction.cumulative_leverage,
                 cumulative_order_type=new_transaction.cumulative_order_type,
@@ -94,7 +109,20 @@ async def adjust_position_endpoint(position_data: TransactionCreate, db: AsyncSe
         )
 
         logger.info("Position adjusted successfully")
-        return TradeResponse(message="Position adjusted successfully")
+        return {
+            "message": "Position adjusted successfully",
+            "data": {
+                "position_id": new_transaction.position_id,
+                "trader_id": new_transaction.trader_id,
+                "trade_pair": new_transaction.trade_pair,
+                "cumulative_leverage": new_transaction.cumulative_leverage,
+                "cumulative_order_type": new_transaction.cumulative_order_type,
+                "cumulative_stop_loss": new_transaction.cumulative_stop_loss,
+                "cumulative_take_profit": new_transaction.cumulative_take_profit,
+                "asset_type": new_transaction.asset_type,
+                "entry_price": new_transaction.entry_price,
+            }
+        }
 
     except Exception as e:
         logger.error(f"Error adjusting position: {e}")
