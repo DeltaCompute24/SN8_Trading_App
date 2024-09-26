53489ea82a5d791e5036cd0729d6630a3c2891ea
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-09-26 11:31:31 +0500

    write a get_taoshi_values function to return profits values

--- src/services/fee_service.py
+++ src/services/fee_service.py
@@ -1,3 +1,12 @@
+from datetime import datetime, timedelta
+
+import redis
+
+from services.api_service import get_profit_and_current_price
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
 def get_assets_fee(asset_type):
     if asset_type == "crypto":
         return 0.001
@@ -5,3 +14,24 @@ def get_assets_fee(asset_type):
         return 0.00007
     else:  # for indices
         return 0.00009
+
+
+def get_taoshi_values(trade_pair, trader_id):
+    key = f"{trade_pair}-{trader_id}"
+    position = redis_client.hget('positions', key)
+    if not position:
+        return 0.0, 0.0, 0.0, 0.0, 0.0
+
+    position = list(position.decode('utf-8'))
+    current_time = datetime.now()
+
+    difference = abs(current_time - position[0])
+    if difference < timedelta(seconds=5):
+        return position[1:]
+
+    price, profit_loss, profit_loss_without_fee, taoshi_profit_loss, taoshi_profit_loss_without_fee = get_profit_and_current_price(
+        trader_id, trade_pair)
+    value = [datetime.now(), price, profit_loss, profit_loss_without_fee, taoshi_profit_loss,
+             taoshi_profit_loss_without_fee]
+    redis_client.hset('positions', f"{trade_pair}-{trader_id}", str(value))
+    return value[1:]
