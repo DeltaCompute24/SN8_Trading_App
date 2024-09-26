c7d502537cee397d67a00eabef35b3528ac73fbb
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-09-26 18:11:32 +0500

    changed fee service logic

--- src/services/fee_service.py
+++ src/services/fee_service.py
@@ -20,20 +20,25 @@ def get_assets_fee(asset_type):
 def get_taoshi_values(trader_id, trade_pair):
     key = f"{trade_pair}-{trader_id}"
     position = redis_client.hget('positions', key)
-    if not position:
-        return 0.0, 0.0, 0.0, 0.0, 0.0, "", ""
+    if position:
+        position = ast.literal_eval(position.decode('utf-8'))
+        current_time = datetime.now()
+        position_time = datetime.strptime(position[0], '%Y-%m-%d %H:%M:%S.%f')
 
-    position = ast.literal_eval(position.decode('utf-8'))
-    current_time = datetime.now()
-    position_time = datetime.strptime(position[0], '%Y-%m-%d %H:%M:%S.%f')
+        difference = abs(current_time - position_time)
+        if difference < timedelta(seconds=5):
+            return position[1:]
 
-    difference = abs(current_time - position_time)
-    if difference < timedelta(seconds=5):
-        return position[1:]
-
-    price, profit_loss, profit_loss_without_fee, taoshi_profit_loss, taoshi_profit_loss_without_fee, uuid, hot_key = get_profit_and_current_price(
-        trader_id, trade_pair)
-    value = [str(datetime.now()), price, profit_loss, profit_loss_without_fee, taoshi_profit_loss,
-             taoshi_profit_loss_without_fee, uuid, hot_key]
-    redis_client.hset('positions', f"{trade_pair}-{trader_id}", str(value))
-    return value[1:]
+        price, profit_loss, profit_loss_without_fee, taoshi_profit_loss, taoshi_profit_loss_without_fee, uuid, hot_key = get_profit_and_current_price(
+            trader_id, trade_pair)
+        value = [str(datetime.now()), price, profit_loss, profit_loss_without_fee, taoshi_profit_loss,
+                 taoshi_profit_loss_without_fee, uuid, hot_key]
+        redis_client.hset('positions', f"{trade_pair}-{trader_id}", str(value))
+        return value[1:]
+    else:
+        price, profit_loss, profit_loss_without_fee, taoshi_profit_loss, taoshi_profit_loss_without_fee, uuid, hot_key = get_profit_and_current_price(
+            trader_id, trade_pair)
+        value = [str(datetime.now()), price, profit_loss, profit_loss_without_fee, taoshi_profit_loss,
+                 taoshi_profit_loss_without_fee, uuid, hot_key]
+        redis_client.hset('positions', f"{trade_pair}-{trader_id}", str(value))
+        return value[1:]
