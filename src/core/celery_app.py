975de566605aa84c9b4c096549b8cb30dc568660
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-09-26 12:29:03 +0500

    comment other celery tasks

--- src/core/celery_app.py
+++ src/core/celery_app.py
@@ -8,25 +8,25 @@ celery_app = Celery(
 
 celery_app.conf.update(
     task_routes={
-        'src.tasks.subscription_manager.manage_subscriptions': {'queue': 'subscription_management'},
-        'src.tasks.subscription_manager.trade_pair_worker': {'queue': 'trade_pair_workers'},
-        'src.tasks.position_monitor_sync.monitor_positions': {'queue': 'position_monitoring'},
+        # 'src.tasks.subscription_manager.manage_subscriptions': {'queue': 'subscription_management'},
+        # 'src.tasks.subscription_manager.trade_pair_worker': {'queue': 'trade_pair_workers'},
+        # 'src.tasks.position_monitor_sync.monitor_positions': {'queue': 'position_monitoring'},
         'src.tasks.listen_for_profit_loss.monitor_taoshi': {'queue': 'monitor_taoshi'},
-        'src.tasks.redis_listener.event_listener': {'queue': 'event_listener'},
+        # 'src.tasks.redis_listener.event_listener': {'queue': 'event_listener'},
     },
     beat_schedule={
-        'manage_subscriptions-every-10-seconds': {
-            'task': 'src.tasks.subscription_manager.manage_subscriptions',
-            'schedule': 10.0,  # every 10 seconds
-        },
-        'monitor_positions-every-1-second': {
-            'task': 'src.tasks.position_monitor_sync.monitor_positions',
-            'schedule': 10.0,  # every 1 second
-        },
-        'redis-listener-every-15-seconds': {
-            'task': 'src.tasks.redis_listener.event_listener',
-            'schedule': 20.0,  # every 20 second
-        },
+        # 'manage_subscriptions-every-10-seconds': {
+        #     'task': 'src.tasks.subscription_manager.manage_subscriptions',
+        #     'schedule': 10.0,  # every 10 seconds
+        # },
+        # 'monitor_positions-every-1-second': {
+        #     'task': 'src.tasks.position_monitor_sync.monitor_positions',
+        #     'schedule': 10.0,  # every 1 second
+        # },
+        # 'redis-listener-every-15-seconds': {
+        #     'task': 'src.tasks.redis_listener.event_listener',
+        #     'schedule': 20.0,  # every 20 second
+        # },
         'monitor_taoshi_every_20_seconds': {
             'task': 'src.tasks.listen_for_profit_loss.monitor_taoshi',
             'schedule': 20.0,  # every 20 second
