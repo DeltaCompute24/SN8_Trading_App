ebf06b6f216bf50b3bed781d55dd5aae720585be
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-09-27 17:53:23 +0500

    added monitor_challenges task in celery core_app

--- src/core/celery_app.py
+++ src/core/celery_app.py
@@ -9,9 +9,9 @@ celery_app = Celery(
 celery_app.conf.update(
     task_routes={
         # 'src.tasks.subscription_manager.manage_subscriptions': {'queue': 'subscription_management'},
-        # 'src.tasks.subscription_manager.trade_pair_worker': {'queue': 'trade_pair_workers'},
         # 'src.tasks.position_monitor_sync.monitor_positions': {'queue': 'position_monitoring'},
         'src.tasks.listen_for_profit_loss.monitor_taoshi': {'queue': 'monitor_taoshi'},
+        # 'src.tasks.listen_for_profit_loss.monitor_challenges': {'queue': 'monitor_challenges'},
         # 'src.tasks.redis_listener.event_listener': {'queue': 'event_listener'},
     },
     beat_schedule={
@@ -31,6 +31,10 @@ celery_app.conf.update(
             'task': 'src.tasks.listen_for_profit_loss.monitor_taoshi',
             'schedule': 3.0,  # every 3 second
         },
+        # 'monitor_challenges_every_3_seconds': {
+        #     'task': 'src.tasks.listen_for_profit_loss.monitor_challenges',
+        #     'schedule': 3.0,  # every 3 second
+        # },
     },
     timezone='UTC',
 )
@@ -38,7 +42,3 @@ celery_app.conf.update(
 celery_app.autodiscover_tasks(['src.tasks'])
 
 # Ensure tasks are loaded
-import src.tasks.subscription_manager
-import src.tasks.position_monitor_sync
-import src.tasks.redis_listener
-import src.tasks.listen_for_profit_loss
