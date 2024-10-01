6704062d75b6ef8752293c59ba8157b6cc49cebf
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-10-01 18:15:45 +0500

    Add the payment model in init file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,5 +5,6 @@ from .challenge import Challenge
 from .monitored_positions import MonitoredPosition
 from .transaction import Transaction
 from .users import Users
+from .payments import Payment
 
-__all__ = ["Base", "Transaction", "MonitoredPosition", "Users", "FirebaseUser", "Challenge"]
+__all__ = ["Base", "Transaction", "MonitoredPosition", "Users", "FirebaseUser", "Challenge", "Payment"]
