3722dc37352ffe37795597147b9052f52b7db99e
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-10-04 12:41:03 +0500

    attach template and image in email

--- src/services/email_service.py
+++ src/services/email_service.py
@@ -1,10 +1,21 @@
 import smtplib
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from fastapi import HTTPException
+from jinja2 import Environment, FileSystemLoader
 
 from src.config import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+
+
+def render_to_string(template_name, context=None):
+    # Set up the Jinja2 environment to look for templates in the current directory
+    env = Environment(loader=FileSystemLoader(searchpath="./"))
+    template = env.get_template(template_name)
+
+    # Render the template with the context and return as a string
+    return template.render(context)
 
 
 def send_mail(receiver, subject, content):
@@ -25,7 +36,17 @@ def send_mail(receiver, subject, content):
         message['Subject'] = subject
 
         # Attach the email body
-        message.attach(MIMEText(content, 'plain'))
+        context = {'email': receiver, 'text': content}
+        html_content = render_to_string('src/templates/email_template.html', context)
+        message.attach(MIMEText(html_content, 'html'))  # HTML version
+
+        image_path = 'src/templates/image1.png'
+        with open(image_path, 'rb') as img_file:
+            img_data = img_file.read()
+            image = MIMEImage(img_data)
+            image.add_header('Content-ID', '<image1>')  # Content-ID to reference in the HTML
+            image.add_header('Content-Disposition', 'inline', filename=image_path)
+            message.attach(image)
 
         # Log the email contents to ensure they're correct
         print(f"Email details: \nFrom: {EMAIL_HOST_USER}\nTo: {receiver}\nSubject: {subject}\nContent: {content}")
