7077d9644f2a543c21dfac51c6370d1c7dbf583c
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-10-10 18:30:27 +0500

    updated retries from 6 to 12

--- src/api/routes/adjust_position.py
+++ src/api/routes/adjust_position.py
@@ -68,7 +68,7 @@ async def adjust_position_endpoint(position_data: TransactionUpdate, db: AsyncSe
             logger.info("Adjustment submitted successfully")
 
             # loop to get the current price
-            for i in range(7):
+            for i in range(12):
                 time.sleep(1)
                 realtime_price, profit_loss, profit_loss_without_fee, taoshi_profit_loss, *taoshi_profit_loss_without_fee = get_taoshi_values(
                     position_data.trader_id,
