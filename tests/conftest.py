0b1f9cc5213f8e52ec3d388a945a1162573e6b5b
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-11-15 15:51:10 +0500

    test initiate position

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,124 @@
-import pytest
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+import os
+from typing import Iterator
+
+import asyncio
+import pytest_asyncio
+from dotenv import load_dotenv
 from httpx import AsyncClient
-from src.main import app
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+
 from src.database import get_db
-from src.models import Base
-from src.config import DATABASE_URL
+from src.main import app
+from src.models.transaction import Transaction
 
-# Set up the database connection
-SQLALCHEMY_DATABASE_URL = DATABASE_URL
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True, echo=True)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+load_dotenv()
 
-# Create a new database session for each test
-@pytest.fixture(scope="function")
-async def session():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    async with TestingSessionLocal() as session:
+DATABASE_URL = os.getenv("DATABASE_URL_TEST")
+
+
+# drop all database every time when test complete
+@pytest_asyncio.fixture
+async def async_db_engine():
+    async_engine = create_async_engine(
+        url=DATABASE_URL,
+        echo=True,
+    )
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+    yield async_engine
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+
+
+# truncate all table to isolate tests
+@pytest_asyncio.fixture
+async def async_db_session(async_db_engine):
+    async_session = (
+        sessionmaker(
+            expire_on_commit=False,
+            autocommit=False,
+            autoflush=False,
+            bind=async_db_engine,
+            class_=AsyncSession,
+        ))
+
+    async with async_session() as session:
+        await session.begin()
+
         yield session
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
 
-# Override the get_db dependency to use the testing session
-@pytest.fixture(scope="function")
-async def async_client(session: AsyncSession):
-    async def _get_test_db():
-        yield session
+        await session.rollback()
 
-    app.dependency_overrides[get_db] = _get_test_db
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        yield ac
-    app.dependency_overrides[get_db] = get_db
-    
+        for table in reversed(SQLModel.metadata.sorted_tables):
+            await session.execute(f'TRUNCATE {table.name} CASCADE;')
+            await session.commit()
+
+
+@pytest_asyncio.fixture
+async def async_client(async_db_session: AsyncSession) -> AsyncClient:
+    def override_get_db() -> Iterator[AsyncSession]:
+        yield async_db_session
+
+    app.dependency_overrides[get_db] = override_get_db
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
+
+@pytest_asyncio.fixture
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture
+def transaction_payload() -> dict:
+    return {
+        "trader_id": 4040,
+        "trade_pair": "BTCUSD",
+        "leverage": 1,
+        "asset_type": "crypto",
+        "stop_loss": 2,
+        "take_profit": 2,
+        "order_type": "LONG"
+    }
+
+
+# Doesn't work because it is not deleted after successful test
+@pytest_asyncio.fixture
+async def transaction_object(async_db_session: AsyncSession) -> Transaction:
+    transaction = Transaction(
+        trader_id=4040,
+        trade_pair="BTCUSD",
+        leverage=0.1,
+        initial_price=58906.0,
+        entry_price=58906.0,  # entry price and initial price will be different if status is pending
+        asset_type="crypto",
+        stop_loss=0.2,
+        take_profit=0.2,
+        order_type="LONG",
+        operation_type="initiate",
+        cumulative_leverage=0.1,
+        cumulative_order_type="LONG",
+        cumulative_stop_loss=0.2,
+        cumulative_take_profit=0.2,
+        average_entry_price=58906.0,
+        status="OPEN",
+        old_status="OPEN",
+        trade_order=1,
+        position_id=1,
+        uuid="2CA263F1-5C94-11E0-84CC-002170FBAC5B",
+        hot_key="5CRwSWfJWnMat1wtUvLTLUJ3fkTTgn1XDC8jVko2H8CmnYC2",
+        limit_order=0.0,
+        source="test",
+        modified_by="4040",
+    )
+    async_db_session.add(transaction)
+    await async_db_session.commit()
+    await async_db_session.refresh(transaction)
+    return transaction
--- tests/test_initiate_position.py
+++ tests/test_initiate_position.py
@@ -1,68 +1,64 @@
-import unittest
-from unittest.mock import AsyncMock, patch
+from unittest.mock import patch, AsyncMock
 
-from httpx import AsyncClient
+import pytest
 
-from src.main import app
+import src
+
+pytestmark = pytest.mark.asyncio
 
 
-class TestInitiatePosition(unittest.IsolatedAsyncioTestCase):
-    async def asyncSetUp(self):
-        self.client = AsyncClient(app=app, base_url="http://test")
-        self.payload = {
-            "trader_id": 4060,
-            "trade_pair": "BTCUSD",
-            "leverage": 1,
-            "asset_type": "crypto",
-            "stop_loss": 2,
-            "take_profit": 2,
-            "order_type": "LONG"
-        }
+class TestInitiatePosition:
 
-    async def asyncTearDown(self):
-        await self.client.aclose()
-
-    async def test_position_already_exist(self):
-        with patch("src.api.routes.initiate_position.get_latest_position", new=AsyncMock(return_value=self.payload)):
-            response = await self.client.post("/trades/initiate-position/", json=self.payload)
+    async def test_position_already_exist(self, async_client, transaction_payload):
+        with (
+            patch(target="src.api.routes.initiate_position.get_latest_position",
+                  new=AsyncMock(return_value=transaction_payload)),
+        ):
+            response = await async_client.post("/trades/initiate-position/", json=transaction_payload)
             # assert response
-            self.assertEqual(response.status_code, 400)
-            self.assertEqual(response.json(),
-                             {"detail": "An open or pending position already exists for this trade pair and trader"})
+            assert response.status_code == 400
+            assert response.json() == {
+                "detail": "An open or pending position already exists for this trade pair and trader"}
 
-    async def test_challenge_not_exist(self):
-        with (patch(target="src.api.routes.initiate_position.get_latest_position", new=AsyncMock(return_value=None)),
-              patch(target="src.api.routes.initiate_position.get_challenge", return_value=None)):
-            response = await self.client.post("/trades/initiate-position/", json=self.payload)
+    async def test_challenge_not_exist(self, async_client, transaction_payload):
+        with (
+            patch(target="src.api.routes.initiate_position.get_latest_position",
+                  new=AsyncMock(return_value=None)),
+            patch('src.api.routes.initiate_position.get_challenge', return_value=None)
+        ):
+            response = await async_client.post("/trades/initiate-position/", json=transaction_payload)
             # assert response
-            self.assertEqual(response.status_code, 500)
-            self.assertEqual(response.json(),
-                             second={
-                                 "detail": f"400: Given Trader ID {self.payload['trader_id']} does not exist in the system!"})
+            assert response.status_code == 500
+            assert response.json() == {
+                "detail": f"400: Given Trader ID {transaction_payload['trader_id']} does not exist in the system!"}
 
-    async def test_submit_trade_failed(self):
-        with (patch(target="src.api.routes.initiate_position.get_latest_position", new=AsyncMock(return_value=None)),
-              patch(target="src.api.routes.initiate_position.get_challenge", return_value="test"),
-              patch(target="src.api.routes.initiate_position.websocket_manager.submit_trade", return_value=False)):
-            response = await self.client.post("/trades/initiate-position/", json=self.payload)
+    async def test_submit_trade_failed(self, async_client, transaction_payload):
+        with (
+            patch(target="src.api.routes.initiate_position.get_challenge", return_value="test"),
+            patch(target="src.api.routes.initiate_position.websocket_manager.submit_trade", return_value=False)
+        ):
+            response = await async_client.post("/trades/initiate-position/", json=transaction_payload)
             # assert response
-            self.assertEqual(response.status_code, 500)
-            self.assertEqual(response.json(), second={"detail": "500: Failed to submit trade"})
+            assert response.status_code == 500
+            assert response.json() == {"detail": "500: Failed to submit trade"}
 
-    async def test_trade_submitted_successfully(self):
-        with (patch(target="src.api.routes.initiate_position.get_latest_position", new=AsyncMock(return_value=None)),
-              patch(target="src.api.routes.initiate_position.get_challenge", return_value="test"),
-              patch(target="src.api.routes.initiate_position.websocket_manager.submit_trade", return_value=True),
-              patch(target="src.api.routes.initiate_position.get_taoshi_values",
-                    return_value=(0, 1, 1, 1, 1, 1, 1, 1, 1))):
+    async def test_trade_submitted_successfully(self, async_client, transaction_payload):
+        with (
+            patch(target="src.api.routes.initiate_position.get_latest_position", new=AsyncMock(return_value=None)),
+            patch(target="src.api.routes.initiate_position.get_challenge", return_value="test"),
+            patch(target="src.api.routes.initiate_position.websocket_manager.submit_trade", return_value=True),
+            patch(target="src.api.routes.initiate_position.get_taoshi_values",
+                  return_value=(0, 1, 1, 1, 1, 1, 1, 1, 1))
+        ):
             # call api
-            response = await self.client.post("/trades/initiate-position/", json=self.payload)
+            response = await async_client.post("/trades/initiate-position/", json=transaction_payload)
             # assert response
-            self.assertEqual(response.status_code, 500)
-            self.assertEqual(response.json(),
-                             second={"detail": "500: Failed to fetch current price for the trade pair"})
+            assert response.status_code == 500
+            assert response.json() == {"detail": "500: Failed to fetch current price for the trade pair"}
+            # get_taoshi_values should be called 12 times because first_price is 0
+            assert src.api.routes.initiate_position.get_taoshi_values.call_count == 12
 
-    async def test_open_position_successfully(self):
+    async def test_open_position_successfully(self, async_client, transaction_payload):
         with (
             patch(target="src.api.routes.initiate_position.get_latest_position", new=AsyncMock(return_value=None)),
             patch(target="src.api.routes.initiate_position.get_challenge", return_value="test"),
@@ -70,10 +66,11 @@ class TestInitiatePosition(unittest.IsolatedAsyncioTestCase):
             patch(target="src.api.routes.initiate_position.get_taoshi_values",
                   return_value=(1, 1, 1, 1, 1, 1, 1, 1, 1)),
             patch("src.api.routes.initiate_position.create_transaction", new=AsyncMock()),
+            patch("src.api.routes.initiate_position.MonitoredPositionCreate", new=AsyncMock()),
             patch("src.api.routes.initiate_position.update_monitored_positions", new=AsyncMock()),
         ):
             # call api
-            response = await self.client.post("/trades/initiate-position/", json=self.payload)
+            response = await async_client.post("/trades/initiate-position/", json=transaction_payload)
             # assert response
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json(), {"detail": "Position initiated successfully"})
+            assert response.status_code == 200
+            assert response.json() == {"message": "Position initiated successfully"}
