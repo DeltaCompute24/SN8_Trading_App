84323eb03f00979a3b785822529f8efed5f28fe8
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-11-28 17:04:02 +0500

    updated celery app for new task changes

--- src/core/celery_app.py
+++ src/core/celery_app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+
 from src.config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 celery_app = Celery(
@@ -11,9 +12,10 @@ celery_app.conf.update(
     task_routes={
         'src.tasks.send_notification.send_notifications': {'queue': 'send_notifications'},
         'src.tasks.position_monitor_sync.monitor_positions': {'queue': 'position_monitoring'},
-        'src.tasks.listen_for_profit_loss.monitor_taoshi': {'queue': 'monitor_taoshi'},
-        'src.tasks.monitor_challenges.monitor_challenges': {'queue': 'monitor_challenges'},
         'src.tasks.redis_listener.event_listener': {'queue': 'event_listener'},
+        'src.tasks.monitor_mainnet_challenges.monitor_mainnet_challenges': {'queue': 'monitor_mainnet_challenges'},
+        'src.tasks.monitor_miner_positions.monitor_miner': {'queue': 'monitor_miner'},
+        'src.tasks.testnet_validator.testnet_validator': {'queue': 'testnet_validator'}
     },
     beat_schedule={
         'send_notifications-every-1-second': {
@@ -28,13 +30,17 @@ celery_app.conf.update(
             'task': 'src.tasks.redis_listener.event_listener',
             'schedule': 15.0,  # every 20 second
         },
-        'monitor_taoshi_every_1_second': {
-            'task': 'src.tasks.listen_for_profit_loss.monitor_taoshi',
-            'schedule': 1.0,  # every 3 second
+        'monitor_mainnet_challenges_every_1_second': {
+            'task': 'src.tasks.monitor_mainnet_challenges.monitor_mainnet_challenges',
+            'schedule': 1.0,  # every 1 second
         },
-        'monitor_challenges_every_5_seconds': {
-            'task': 'src.tasks.monitor_challenges.monitor_challenges',
-            'schedule': 5.0,  # every 10 second
+        'monitor_miner_every_1_second': {
+            'task': 'src.tasks.monitor_miner_positions.monitor_miner',
+            'schedule': 1.0,  # every 1 second
+        },
+        'testnet_validator_every_1_second': {
+            'task': 'src.tasks.testnet_validator.testnet_validator',
+            'schedule': 1.0,  # every 1 second
         },
     },
     timezone='UTC',
@@ -43,8 +49,3 @@ celery_app.conf.update(
 celery_app.autodiscover_tasks(['src.tasks'])
 
 # Ensure tasks are loaded
-import src.tasks.position_monitor_sync
-import src.tasks.redis_listener
-import src.tasks.listen_for_profit_loss
-import src.tasks.monitor_challenges
-import src.tasks.send_notification
