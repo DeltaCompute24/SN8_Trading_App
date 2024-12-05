3aa0b234b29e8a1e828715ce25611a1a9daebc52
sakhawatdevelopment <sakhawat.ali.development@gmail.com>
2024-12-05 19:26:05 +0500

    add the tournamnet service

--- src/services/tournament_service.py
+++ src/services/tournament_service.py
@@ -1,9 +1,15 @@
+import threading
+
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import and_
 
 from src.models.tournament import Tournament
 from src.schemas.tournament import TournamentCreate, TournamentUpdate
+from src.services.email_service import send_mail
+from src.services.payment_service import create_challenge, register_and_update_challenge, create_payment_entry
+from src.services.user_service import get_firebase_user
 
 
 def create_tournament(db: Session, tournament_data: TournamentCreate):
@@ -46,3 +52,56 @@ def delete_tournament(db: Session, tournament_id: int):
         db.delete(tournament)
         db.commit()
     return tournament
+
+
+def register_payment(db, tournament_id, firebase_id, amount, referral_code):
+    # Validate Firebase User
+    firebase_user = get_firebase_user(db, firebase_id)
+    if not firebase_user or not firebase_user.username:
+        raise HTTPException(status_code=400, detail="Invalid Firebase user data")
+
+    # Fetch Tournament
+    tournament = db.query(Tournament).filter(Tournament.id == tournament_id).first()
+    if not tournament:
+        raise HTTPException(status_code=404, detail="Tournament Not Found")
+
+    # Prepare Payment Data
+    payment_data = {
+        "amount": amount,
+        "referral_code": referral_code,
+        "step": 2,
+        "phase": 1,
+        "firebase_id": firebase_id,
+    }
+
+    # Create Challenge and Associate with Tournament
+    new_challenge = create_challenge(
+        db,
+        payment_data=payment_data,
+        network="test",  # You can change this to 'main' if needed
+        user=firebase_user,
+        challenge_status="Tournament",
+        step=2,
+        phase=1
+    )
+
+    # Thread to handle challenge updates
+    thread = threading.Thread(target=register_and_update_challenge, args=(new_challenge.id, "Tournament"))
+    thread.start()
+
+    # Create Payment Entry
+    new_payment = create_payment_entry(db, payment_data, new_challenge)
+
+    # Send Confirmation Email
+    send_mail(
+        receiver=firebase_user.email,
+        template_name="EmailTemplate.html",
+        subject="Tournament Registration Confirmed",
+        content=f"You are successfully registered in the tournament{tournament.name}",
+        context={
+            "username": firebase_user.username,
+            "tournament": tournament.name,
+        },
+    )
+
+    return {"message": f"Tournament Payment Registered Successfully"}
