3757950670b65d31dfcfacef7db859b9f5099155
HatimZ <hatimzahidhusain@gmail.com>
2024-12-16 23:45:33 +0500

    Added deployment and Trade Hotfix

--- src/api/routes/initiate_position.py
+++ src/api/routes/initiate_position.py
@@ -63,7 +63,7 @@ async def initiate_position(position_data: TransactionCreate, db: AsyncSession =
             logger.info("Trade submitted successfully")
 
             # loop to get the current price
-            for i in range(12):
+            for i in range(20):
                 time.sleep(1)
                 first_price, profit_loss, profit_loss_without_fee, taoshi_profit_loss, taoshi_profit_loss_without_fee, uuid, hot_key, len_order, average_entry_price = get_taoshi_values(
                     position_data.trader_id,
